036d650fadf479e6413293bffdf5736675c21c78
İsmahan Arı <ismhnarii@gmail.com>
2023-05-30 18:05:15 +0300

    fonk

--- js/05-fonk.js
+++ js/05-fonk.js
@@ -11,3 +11,25 @@ function Hello(){
 
 Hello()
 
+function printHello(name){ //parametre aldı 
+    console.log("merhaba " + name)
+}
+
+printHello("ismahan") // fonksiyon parametre ile çağrıldı
+
+function mesajver(){
+    alert("Herkese Merhabalar!")
+}
+mesajver()
+
+function MesajVer(ad,soyad){
+    console.log(`merhaba ${ad} ${soyad}`);
+}
+MesajVer("ismahan","Arı")
+
+//Geri dönüş değeri
+function topla(sayi1,sayi2){
+    return sayi1 + sayi2
+}
+let sonuc = topla(10,20);
+console.log(sonuc)
