0db1a8873a17f4d9f98eec571133483023361f21
İsmahan Arı <ismhnarii@gmail.com>
2023-08-11 20:06:45 +0300

    .

--- js_uyg/uyg.js
+++ js_uyg/uyg.js
@@ -5,9 +5,9 @@
     const liste = document.querySelector("#myTable") 
 
     btn.onclick = function(){
-       
         let td_eleman=document.createElement("input_ekle");
         td_eleman.textContent = input_ekle.value
+       if (input_ekle.value.length > 0) {  // boş ekleme yapmaz!
 
         let tr=document.createElement("tr");
         tr.appendChild(td_eleman);
@@ -15,25 +15,36 @@
         liste.appendChild(tr);
 
         input_ekle.value = "";
+       }
 
+       else{
+        alert("lütfen birşeyler yazın !")
+       }
     }
 
-
+    //sondan başa doğru sildirir.
     function deleteRow(ele){
-        var table = document.getElementById(`myTable`)
+        var table = document.getElementById(`myTable`) 
         var rowcount = table.rows.length
 
         if (rowcount<1){
             alert("silinecek satır yok")
             return
         }
-        if(ele){
-            ele.parentNode.parentNode.remove()
-            //belirli satırları sil
-             //çalışmıyor !
-
-        }
         else{
             table.deleteRow(rowcount-1)
         }
     }
+
+
+    // Üzerine çift tıklanınca siliniyor ve güncelleme işlemi yapılıyor.
+    document.getElementById(`myTable`).addEventListener(`dblclick`, function(event){
+        
+        event.target.parentNode.removeChild(event.target)
+        var yeni_bilgi = prompt("lütfen yeni bilgi giriniz : ")
+        let tablo = document.querySelector("#myTable")
+        tablo.innerHTML = `${tablo.innerHTML} ${yeni_bilgi}`
+        
+    })
+
+    
